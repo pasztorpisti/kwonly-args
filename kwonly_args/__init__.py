53ad0fe5b63a9065000a2c91940f1b9be9ae855d
Istvan Pasztor <pasztorpisti@gmail.com>
2016-04-09 16:26:43 +0100

    bumping version to 1.0.8

--- kwonly_args/__init__.py
+++ kwonly_args/__init__.py
@@ -16,7 +16,7 @@ __all__ = ['first_kwonly_arg', 'KWONLY_REQUIRED', 'FIRST_DEFAULT_ARG', 'kwonly_d
 #                  case increase only version_info[2].
 # version_info[2]: Increase in case of bugfixes. Also use this if you added new features
 #                  without modifying the behavior of the previously existing ones.
-version_info = (1, 0, 7)
+version_info = (1, 0, 8)
 __version__ = '.'.join(str(n) for n in version_info)
 __author__ = 'István Pásztor'
 __license__ = 'MIT'
