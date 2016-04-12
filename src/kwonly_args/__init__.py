7603c3a0f0c2d37d02c7220492de248f65ff0451
Istvan Pasztor <pasztorpisti@gmail.com>
2016-04-12 01:37:17 +0100

    bumping version to 1.0.10

--- src/kwonly_args/__init__.py
+++ src/kwonly_args/__init__.py
@@ -22,7 +22,7 @@ __all__ = ['first_kwonly_arg', 'KWONLY_REQUIRED', 'FIRST_DEFAULT_ARG', 'kwonly_d
 #                  case increase only version_info[2].
 # version_info[2]: Increase in case of bugfixes. Also use this if you added new features
 #                  without modifying the behavior of the previously existing ones.
-version_info = (1, 0, 9)
+version_info = (1, 0, 10)
 __version__ = '.'.join(str(n) for n in version_info)
 __author__ = 'István Pásztor'
 __license__ = 'MIT'
